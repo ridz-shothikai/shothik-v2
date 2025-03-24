4247ae651438c979b6ab45ce76e3c36e5b460a49
ridz-shothikai <128675828+ridz-shothikai@users.noreply.github.com>
2025-03-24 08:07:48 +0000

    updates

--- src/components/tools/humanize/TopNavigation.jsx
+++ src/components/tools/humanize/TopNavigation.jsx
@@ -1,5 +1,19 @@
-import { Lock } from "@mui/icons-material";
-import { Slider, Stack, Tab, Tabs, Tooltip } from "@mui/material";
+import { Lock, Diamond } from "@mui/icons-material";
+import {
+  Slider,
+  Stack,
+  Tab,
+  Tabs,
+  Tooltip,
+  Popover,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
+import { useState } from "react";
+
+import { useRouter } from "next/navigation";
+
 const models = ["Panda", "Raven"];
 
 const TopNavigation = ({
@@ -11,6 +25,31 @@ const TopNavigation = ({
   setCurrentLength,
   LENGTH,
 }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const router = useRouter()
+
+  const handleTabClick = (event, tab) => {
+    const isLocked = !/pro_plan|unlimited/.test(userPackage) && tab === "Raven";
+
+    if (isLocked) {
+      setShalowAlert(true);
+      setAnchorEl(event.currentTarget);
+    } else {
+      setShalowAlert(false);
+      setAnchorEl(null);
+    }
+
+    setModel(tab);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "upgrade-popover" : undefined;
+
   return (
     <Stack
       direction='row'
@@ -31,44 +70,41 @@ const TopNavigation = ({
           },
         }}
       >
-        {models.map((tab) => (
-          <Tab
-            key={tab}
-            value={tab}
-            color='#00A76F'
-            sx={{
-              color: "text.secondary",
-              "&.Mui-selected": {
-                color: "primary.main",
-              },
-            }}
-            label={
-              <Tooltip title='Model' arrow placement='top'>
-                {tab}
-              </Tooltip>
-            }
-            icon={
-              !/pro_plan|unlimited/.test(userPackage) && tab === "Raven" ? (
-                <Lock sx={{ width: 12, height: 12 }} />
-              ) : undefined
-            }
-            onClick={() => {
-              if (!/pro_plan|unlimited/.test(userPackage) && tab === "Raven") {
-                setShalowAlert(true);
-              } else {
-                setShalowAlert(false);
+        {models.map((tab) => {
+          const isLocked =
+            !/pro_plan|unlimited/.test(userPackage) && tab === "Raven";
+          return (
+            <Tab
+              key={tab}
+              value={tab}
+              color='#00A76F'
+              sx={{
+                color: "text.secondary",
+                "&.Mui-selected": {
+                  color: "primary.main",
+                },
+              }}
+              label={
+                <Tooltip title='Model' arrow placement='top'>
+                  {tab}
+                </Tooltip>
               }
-              setModel(tab);
-            }}
-          />
-        ))}
+              icon={
+                isLocked ? <Lock sx={{ width: 12, height: 12 }} /> : undefined
+              }
+              onClick={(e) => handleTabClick(e, tab)}
+            />
+          );
+        })}
       </Tabs>
 
       <Slider
         style={{ width: "150px" }}
         aria-label='Length'
         getAriaValueText={(value) => LENGTH[value]}
-        value={Object.keys(LENGTH).find((key) => LENGTH[key] === currentLength)}
+        value={Object.keys(LENGTH).find(
+          (key) => LENGTH[key] === currentLength
+        )}
         marks
         step={20}
         min={20}
@@ -90,6 +126,48 @@ const TopNavigation = ({
           },
         }}
       />
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        disableRestoreFocus
+        PaperProps={{
+          sx: {
+            p: 2,
+            borderRadius: 2,
+            boxShadow: 3,
+            width: 300,
+            textAlign: "center",
+          },
+        }}
+      >
+        
+        <Typography variant='body2' sx={{ mt: 1, mb: 2, color: 'text.secondary' }}>
+          Unlock advanced features and enhance your humanize experience.
+        </Typography>
+        <Button
+          variant='contained'
+          color='success'
+          size='small'
+          startIcon={<Diamond />}
+          sx={{ borderRadius: 2, textTransform: "none", fontWeight: 600 }}
+          onClick={e => {
+            router.push("/pricing?redirect=/humanize-gpt")
+          }}
+        >
+          Upgrade To Premium
+        </Button>
+      </Popover>
     </Stack>
   );
 };
